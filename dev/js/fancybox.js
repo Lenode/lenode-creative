df145baf7823b39e91bb2f32939a0161b36b2ebb
John Watson <lenode@gmail.com>
2018-05-02 21:57:45 +0200

    styling changes

--- dev/js/fancybox.js
+++ dev/js/fancybox.js
@@ -4,3 +4,11 @@ $(".fancybox").fancybox({
         return;
     }
 });
+
+$(function(){
+    $('a').each(function(){
+        if ($(this).prop('href') == window.location.href) {
+            $(this).addClass('active'); $(this).parents('li').addClass('active');
+        }
+    });
+});
