1471af9b427f93b3a77ed816fcb4ef44011876cc
John Watson <lenode@gmail.com>
2019-08-28 14:27:04 -0600

    Added B&H and updated text; Fixed fancybox issues

--- dev/_site/js/fancybox.js
+++ dev/_site/js/fancybox.js
@@ -1,14 +1,25 @@
-$(".fancybox").fancybox({
-    afterClose : function() {
-        location.reload();
-        return;
-    }
-});
+// $(".fancybox").fancybox({
+//     afterClose : function() {
+//         location.reload();
+//         return;
+//     }
+// });
 
-$(function(){
-    $('a').each(function(){
-        if ($(this).prop('href') == window.location.href) {
-            $(this).addClass('active'); $(this).parents('li').addClass('active');
-        }
-    });
+// $(".fancybox").fancybox({
+//     closeClick: true
+// }).trigger("click"); // you can chain fancybox and trigger methods
+
+// $(function(){
+//     $('a').each(function(){
+//         if ($(this).prop('href') == window.location.href) {
+//             $(this).addClass('active'); $(this).parents('li').addClass('active');
+//         }
+//     });
+// });
+
+$(document).ready(function() {
+	$(".fancybox").fancybox({
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
 });
--- src/js/fancybox.js
+++ src/js/fancybox.js
@@ -1,6 +1,17 @@
-$(".fancybox").fancybox({
-    afterClose : function() {
-        location.reload();
-        return;
-    }
+// $(".fancybox").fancybox({
+//     afterClose : function() {
+//         location.reload();
+//         return;
+//     }
+// });
+
+// $(".fancybox").fancybox({
+//     closeClick: true
+// }).trigger("click"); // you can chain fancybox and trigger methods
+
+$(document).ready(function() {
+	$(".fancybox").fancybox({
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
 });
